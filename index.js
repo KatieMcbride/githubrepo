f5adf15ae30bf7733d7ea2d49940acef1a815437
Katie McBride <mcbride.katieb@gmail.com>
2019-11-11 22:16:04 -0600

    outline for prompt and pdf

---
+++ index.js
@@ -0,0 +1,23 @@
+function init(){
+    // generate static html 
+    const hcHtml = `
+    <!DOCTYPE html>
+        <html lang="en">
+    <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    </head>
+    <body>
+    <p>We have HTML file</p>
+    
+    </body>
+    </html>`
+    // convert that html to a pdf
+    // save that pdf to the folder
+}
+
+
+// Initial state
+init():
