e3d31358dd4ab4d02ab129b60c3316913276e260
Katie McBride <mcbride.katieb@gmail.com>
2019-11-12 13:59:20 -0600

    updated syntax

--- index.js
+++ index.js
@@ -1,6 +1,29 @@
-function init(){
+const inquirer = require("inquirer");
+const axios = require("axios");
+const fs = require("fs");
+const util = require('util');
+const pdf = require('html-pdf');
+
+const writeFileAsync = util.promisify(fs.writeFile);
+
+inquirer
+    .prompt([
+        {
+        type: 'input',
+        message: 'What is your GitHub name?',
+        name: 'username',
+        },
+        {
+        type: 'input',
+        message: 'What is your favorite color?',
+        color: 'color', 
+        },
+    ])
+    .then(answers => {
+        console.log(answers);
+
     // generate static html 
-    const hcHtml = `
+     const hcHtml = `
     <!DOCTYPE html>
         <html lang="en">
     <head>
@@ -10,14 +33,28 @@ function init(){
     <title>Document</title>
     </head>
     <body>
-    <p>We have HTML file</p>
-    
+    <h1>${answers.name}</h1>
+    <h2>${answers.color}</h2>
     </body>
-    </html>`
+    </html>`;
+
+    return writeFileAsync('index.html', hcHtml);
+})
+.then(() => {
+    // read from file system
+    var html = fs.readFileSync('./index.html','utf8');
+    var options = {format:'Letter'};
+
+    // convert to pdf
+    pdf.create(html, options).toFile('./github.pdf', function(err, res){
+        if (err) return console.log(err);
+        console.log(res);
+    });
+});
     // convert that html to a pdf
     // save that pdf to the folder
-}
 
 
-// Initial state
-init():
+
+// // Initial state
+// init();
