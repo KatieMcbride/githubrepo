4464407f1e74b41aa6390724b7c9f71821adf00b
Katie McBride <mcbride.katieb@gmail.com>
2019-11-12 15:21:42 -0600

    got public repos

--- index.js
+++ index.js
@@ -13,44 +13,61 @@ inquirer
         message: 'What is your GitHub name?',
         name: 'username',
         },
-        {
-        type: 'input',
-        message: 'What is your favorite color?',
-        color: 'color', 
-        },
+        // {
+        // type: 'input',
+        // message: 'What is your favorite color?',
+        // color: 'color', 
+        // },
     ])
-    .then(answers => {
-        console.log(answers);
+    .then(({ username }) => {
+        const queryUrl = `https://api.github.com/users/${username}`;
+    
+        axios.get(queryUrl).then(res => {
+            
+            const publicRepos = res.data.public_repos
+        
+            });
+        //   const repoNames = res.data.map(repo => {
+            
+        //   })
+        //   repoNamesStr = repoNames.join('\n');
+    
+        //   fs.writeFile('repo.txt', repoNamesStr, err =>{
+        //     console.log('touchdown');
+        //   });
+        });
+    
+    
 
-    // generate static html 
-     const hcHtml = `
-    <!DOCTYPE html>
-        <html lang="en">
-    <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-    </head>
-    <body>
-    <h1>${answers.name}</h1>
-    <h2>${answers.color}</h2>
-    </body>
-    </html>`;
+//     // generate static html 
+//      const hcHtml = `
+//     <!DOCTYPE html>
+//         <html lang="en">
+//     <head>
+//     <meta charset="UTF-8">
+//     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+//     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+//     <title>Document</title>
+//     </head>
+//     <body>
+//     <h1>${answers.name}</h1>
+//     <h2>${answers.color}</h2>
+//     </body>
+//     </html>`;
 
-    return writeFileAsync('index.html', hcHtml);
-})
-.then(() => {
+//     return writeFileAsync('index.html', hcHtml);
+// })
+// .then(() => {
     // read from file system
-    var html = fs.readFileSync('./index.html','utf8');
-    var options = {format:'Letter'};
+    // var html = fs.readFileSync('./index.html','utf8');
+    // var options = {format:'Letter'};
 
     // convert to pdf
-    pdf.create(html, options).toFile('./github.pdf', function(err, res){
-        if (err) return console.log(err);
-        console.log(res);
-    });
-});
+//     pdf.create(html, options).toFile('./github.pdf', function(err, res){
+//         if (err) return console.log(err);
+//         console.log(res);
+//     });
+// });
     // convert that html to a pdf
     // save that pdf to the folder
 
